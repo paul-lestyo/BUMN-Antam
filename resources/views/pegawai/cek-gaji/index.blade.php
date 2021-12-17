85d2b7a4ee7479b25a3ea5e2eb5612a118e2d676
VincentMulyadi <71622488+VincentMulyadi@users.noreply.github.com>
2021-12-17 13:37:48 +0700

    Update View Cek Gaji

--- resources/views/pegawai/cek-gaji/index.blade.php
+++ resources/views/pegawai/cek-gaji/index.blade.php
@@ -4,71 +4,116 @@
 {{ $title }}
 @endsection
 
-
 @section('content')
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0">Cek Gaji</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ route('pegawai.dashboard') }}">Pegawai</a></li>
-                        <li class="breadcrumb-item active">Cek Gaji</li>
-                    </ol>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-            @if(Session::has('sukses'))
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                <strong>Sukses!</strong> {{ session('sukses') }}
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            @endif
-        </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Cek Gaji</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="{{ route('pegawai.dashboard') }}">Pegawai</a></li>
+              <li class="breadcrumb-item active">Cek Gaji</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
 
-    <!-- Main content -->
-    <div class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <table class="table">
-                                <thead class="thead-light text-center">
-                                    <th>Nama</th>
-                                    <th>Golongan</th>
-                                    <th>Jumlah Presensi</th>
-                                    <th>Total Gaji</th>
-                                </th>
-                                <tbody class="text-center">
-                                    {{-- @foreach ($pengajuan_cuti as $item)
-                                    <tr>
-                                        <td class="font-weight-bold">{{ $loop->iteration }}.</td>
-                                        <td>{{ $item->started_at}}</td>
-                                        <td>{{ $item->end_at}}</td>
-										<td>{{ $item->status}}</td>
-                                        <td>{{ $item->keterangan}}</td>
-                                    </tr>
-                                    @endforeach --}}
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-                <!-- /.col-md-6 -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <div class="callout callout-info">
+              <h5><i class="fas fa-info"></i> Note:</h5>
+              This page has been enhanced for printing. Click the print button at the bottom of the invoice to test.
             </div>
-            <!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
+
+
+            <!-- Main content -->
+            <div class="invoice p-3 mb-3">
+              <!-- title row -->
+              <div class="row">
+                <div class="col-12">
+                  <h4>
+                    <i class="fas fa-globe"></i> PT ANEKA TAMBANG (ANTAM) TBK
+                    <small class="float-right">Date: 2/10/2014</small>
+                  </h4>
+                </div>
+                <!-- /.col -->
+              </div>
+              <!-- info row -->
+              <div class="row invoice-info">
+                <div class="col-sm-4 invoice-col">
+                  From
+                  <address>
+                    <strong>Admin, TBK.</strong><br>
+                    Jl. Letjen. T.B. Simatupang No. 1<br>
+                    Lingkar Selatan, Tanjung Barat<br>
+                    Jakarta 12530, Indonesia<br>
+                    corsec@antam.com
+                  </address>
+                </div>
+                <!-- /.col -->
+                <div class="col-sm-4 invoice-col">
+                  To
+                  <address>
+                    <strong>John Doe</strong><br>
+                    795 Folsom Ave, Suite 600<br>
+                    San Francisco, CA 94107<br>
+                    Phone: (555) 539-1037<br>
+                    Email: john.doe@example.com
+                  </address>
+                </div>
+              <!-- /.row -->
+
+              <!-- Table row -->
+              <div class="row">
+                <div class="col-12 table-responsive">
+                  <table class="table table-striped">
+                    <thead>
+                    <tr>
+                      <th>No</th>
+                      <th>Gaji Pokok</th>
+                      <th>Persentase Kehadiran</th>
+                      <th>Total Gaji</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>Call of Duty</td>
+                      <td>455-981-221</td>
+                      <td>100000000</td>
+                    </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <!-- /.col -->
+              </div>
+              <!-- /.row -->
+
+              <!-- this row will not appear when printing -->
+              <div class="row no-print">
+                <div class="col-12">
+                  <a href="cek-gaji-print.html" target="_blank" class="btn btn-default"><i class="fas fa-print"></i> Print</a>
+
+                  <button type="button" class="btn btn-primary float-right" style="margin-right: 5px;">
+                    <i class="fas fa-download"></i> Generate PDF
+                  </button>
+                </div>
+              </div>
+            </div>
+            <!-- /.invoice -->
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
     <!-- /.content -->
-</div>
-<!-- /.content-wrapper -->
+  </div>
+  <!-- /.content-wrapper -->
 @endsection
